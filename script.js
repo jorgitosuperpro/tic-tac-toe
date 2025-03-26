07908896a3c98594ced32875efda0281c21f4c3f
Jorge <srcoke98@gmail.com>
2025-03-26 16:17:20 +0100

    Depends on Player's turn mark spaces

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const gameboard = (function createGameboard() {
         grid.push(createCell(i));
     }    
 
+    const winCondition = false;
     const getGrid = () => grid;
     const printGrid = function() {
         let grid_string = "";
@@ -18,16 +19,21 @@ const gameboard = (function createGameboard() {
         return console.log(grid_string);
     }
 
-    const draw = function() {
+    const draw = function(player1, player2) {
         const position = prompt("Tell which cell to draw: 1-9");
         if (grid[position-1].state === "-") {
-            grid[position-1].state = "x";
-            printGrid(); 
+            if (player1.myTurn) {
+                grid[position-1].state = player1.my_symbol;
+                printGrid(); 
+            } else {
+                grid[position-1].state = player2.my_symbol;
+                printGrid(); 
+            }
         }else {
             console.log("You can't draw this cell.")
         }
     }
-    return {getGrid, printGrid, draw};
+    return {getGrid, printGrid, draw, winCondition};
 })();
 
 function createCell(i) {
@@ -37,14 +43,33 @@ function createCell(i) {
     return {cell_id, state};
 }
 
-function createPlayer(name) {
+function createPlayer(name, symbol) {
     const game_name = "Player " + name;
     let wins = 0;
+    const my_symbol = symbol+"";
     const getWins = () => wins;
     const giveWins = () => wins++;
-
-    return {game_name, getWins, giveWins};
+    const myTurn = () => false;
+    return {game_name, my_symbol, getWins, giveWins, myTurn};
 }
 
-const player1 = createPlayer("1");
-const player2 = createPlayer("2");
+const player1 = createPlayer("1", "O");
+const player2 = createPlayer("2", "X");
+
+player1.myTurn = true;
+gameboard.printGrid();
+while(gameboard.winCondition === false) {
+    if (player1.myTurn === true) {
+        gameboard.draw(player1, player2);
+        player1.myTurn = false;
+        player2.myTurn = true;
+        console.log("State of Player's 1 turn: " + player1.myTurn)
+        console.log("State of Player's 2 turn: " + player2.myTurn)
+    } else if (player2.myTurn === true) {
+        gameboard.draw(player1, player2);
+        player2.myTurn = false;
+        player1.myTurn = true;
+        console.log("State of Player's 1 turn: " + player1.myTurn)
+        console.log("State of Player's 2 turn: " + player2.myTurn)
+    }
+}
