bac77275a2e65691b34afe006a408d6fd889fdbc
Jorge <srcoke98@gmail.com>
2025-04-01 01:08:19 +0200

    Logic for preventing players from marking occupied squares

--- script.js
+++ script.js
@@ -27,15 +27,13 @@ const gameboard = (function createGameboard() {
         return console.log(grid_string);
     }
     //draw function to mark cells depending on which player's turn
-    const draw = function(player1, player2) {
-        let position_row = prompt("Tell which row to draw: 0-2");
-        let position_column = prompt("Tell which columnt to draw: 0-2");
-        if (grid[position_row][position_column].state === "-") {
+    const draw = function(player1, player2, posRow, posCol){
+        if (grid[posRow][posCol].state === "-") {
             if (player1.myTurn) {
-                grid[position_row][position_column].state = player1.my_symbol;
-                printGrid(); 
+                grid[posRow][posCol].state = player1.my_symbol;
+                printGrid();
             } else {
-                grid[position_row][position_column].state = player2.my_symbol;
+                grid[posRow][posCol].state = player2.my_symbol;
                 printGrid(); 
             }
         }else {
@@ -59,6 +57,7 @@ const gameboard = (function createGameboard() {
                 gameboard.winCondition = true;
                 player.giveWins();
                 console.log(player.game_name + " wins.")
+                leaderboard.innerHTML = "Player 1 O: " + player1.getWins() + " Player 2 X: " + player2.getWins();
                 break;
             }
         }
@@ -77,6 +76,7 @@ const gameboard = (function createGameboard() {
                 gameboard.winCondition = true;
                 player.giveWins();
                 console.log(player.game_name + " wins.")
+                leaderboard.innerHTML = "Player 1 O: " + player1.getWins() + " Player 2 X: " + player2.getWins();
                 break;
             }
         }
@@ -94,6 +94,7 @@ const gameboard = (function createGameboard() {
             gameboard.winCondition = true;
             player.giveWins();
             console.log(player.game_name + " wins.")
+            leaderboard.innerHTML = "Player 1 O: " + player1.getWins() + " Player 2 X: " + player2.getWins();
         } else {
             //check diagonal right
             isMySymbol = false;
@@ -109,6 +110,7 @@ const gameboard = (function createGameboard() {
                 gameboard.winCondition = true;
                 player.giveWins();
                 console.log(player.game_name + " wins.")
+                leaderboard.innerHTML = "Player 1 O: " + player1.getWins() + " Player 2 X: " + player2.getWins();
             }
         }
         //is it a tie?
@@ -117,7 +119,10 @@ const gameboard = (function createGameboard() {
         if (isATie) gameboard.winCondition = true;
     }
     //reset grid
-    const reset = ()=> initial_grid();
+    const reset = function() {
+        initial_grid();
+        //quitar child html
+    }
     //continue another game
     const nextGame = () => winCondition = false;
     return {getGrid, initial_grid, printGrid, draw, winCondition, checkWinCondition, reset, nextGame};
@@ -134,52 +139,119 @@ function createPlayer(name, symbol) {
     const game_name = "Player " + name;
     let wins = 0;
     const my_symbol = symbol+"";
+    let my_image;
+    if (symbol === "X") {
+        my_image = "images/cross.svg";
+    } else {
+        my_image = "images/circle.svg";
+    }
     const getWins = () => wins;
     const giveWins = () => wins++;
     const myTurn = () => false;
-    return {game_name, my_symbol, getWins, giveWins, myTurn};
+    return {game_name, my_symbol, getWins, giveWins, myTurn, my_image};
 }
 
-//DOM
 const player1 = createPlayer("1", "O");
 const player2 = createPlayer("2", "X");
-nextGame();
-function nextGame() {
+play();
+const leaderboard = document.querySelector(".leaderboard");
+leaderboard.innerHTML = "Player 1 O: " + player1.getWins() + " Player 2 X: " + player2.getWins();
+
+
+function play() {
+    createGrid();
+    createSymbol(player1, player2);
+}
+
+function createGrid() {
     gameboard.initial_grid();
     player1.myTurn = true;
     gameboard.printGrid();
-    gameboard.winCondition = false;
-    gameboard.reset();
-    while(gameboard.winCondition === false) {
-        createDisplay();
-        if (player1.myTurn === true) {
-            gameboard.draw(player1, player2);
-            player1.myTurn = false;
-            player2.myTurn = true;
-            gameboard.checkWinCondition(player1);
-        } else if (player2.myTurn === true) {
-            gameboard.draw(player1, player2);
-            player2.myTurn = false;
-            player1.myTurn = true;
-            gameboard.checkWinCondition(player2);
-        }
+    const container = document.querySelector(".gameboard");
+    container.style.setProperty('--grid-rows', 3);
+    container.style.setProperty('--grid-cols', 3);
+    let cell;
+    for (let i = 0; i < 9; i++) {
+        cell = document.createElement("div");
+        cell.setAttribute("id", i);
+        container.appendChild(cell).className = "grid-item";
     }
 }
 
-
-function createDisplay() {
-    const main_content = document.querySelector(".main-content");
-    const display = document.createElement("div");
-    main_content.appendChild(display);
-    display.setAttribute("class", "gameboard");
-    let displayGrid = document.createElement("div");
-    displayGrid.innerHTML = "";
-    displayGrid.setAttribute("class", "grid");
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            displayGrid.innerHTML += gameboard.getGrid()[i][j].state;
+function createSymbol(player1, player2) {
+    let gridItems = document.querySelectorAll(".grid-item");
+    function printSymbol(event) {
+        const item = event.target;
+    
+        if (gameboard.winCondition) {
+            removeEvent(gridItems, printSymbol); 
+            return; 
         }
-        displayGrid.innerHTML += "<br>";
+    
+        const myimage = document.createElement("img");
+        item.appendChild(myimage);
+        itemIsClicked(item);
+        if (player1.myTurn) {
+            myimage.setAttribute("src", player1.my_image);
+            player1.myTurn = false;
+            player2.myTurn = true;
+        } else {
+            myimage.setAttribute("src", player2.my_image);
+            player1.myTurn = true;
+            player2.myTurn = false;
+        }
+        gameboard.checkWinCondition(player1);
+        gameboard.checkWinCondition(player2);
+        item.removeEventListener("click", printSymbol);
     }
-    display.appendChild(displayGrid);
+    gridItems.forEach(item => {
+        item.addEventListener("click", printSymbol);
+    });
+    const reset = document.querySelector("button");
+    reset.addEventListener("click", function removeImagesAndEventListener() {
+        gameboard.reset();
+        gridItems.forEach(item => {
+            if(item.hasChildNodes()) {
+                item.removeChild(item.firstChild);
+            }
+        });
+        const container = document.querySelector(".gameboard");
+        while(container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+        }
+        gameboard.winCondition = false;
+        player1.myTurn = true;
+        player2.myTurn = false;
+        createGrid();
+        createSymbol(player1, player2);
+    });
+}
+
+function itemIsClicked(item) {
+    let id = item.getAttribute("id");
+    if (id == 0) {
+        gameboard.draw(player1, player2, 0, 0);
+    } else if (id == 1) {
+        gameboard.draw(player1, player2, 0, 1);
+    } else if (id == 2) {
+        gameboard.draw(player1, player2, 0, 2);
+    } else if (id == 3) {
+        gameboard.draw(player1, player2, 1, 0);
+    } else if (id == 4) {
+        gameboard.draw(player1, player2, 1, 1);
+    } else if (id == 5) {
+        gameboard.draw(player1, player2, 1, 2);
+    } else if (id == 6) {
+        gameboard.draw(player1, player2, 2, 0);
+    } else if (id == 7) {
+        gameboard.draw(player1, player2, 2, 1);
+    } else if (id == 8) {
+        gameboard.draw(player1, player2, 2, 2);
+    }
+}
+
+function removeEvent(gridItems, printSymbol) {
+    gridItems.forEach(item => {
+        item.removeEventListener("click", printSymbol);
+    })
 }
